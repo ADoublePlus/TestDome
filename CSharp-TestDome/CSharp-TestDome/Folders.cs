14b35da077c8816f7eaef8638dfdb597ab35d838
John Wonson <jmpwonson@gmail.com>
2020-12-13 13:24:04 +1100

    Folders.cs added along with reference update
    Implementation update: "Folders" question added to project. A reference update to allow 'System.Xml.Linq' to be cited was made.

---
+++ CSharp-TestDome/CSharp-TestDome/Folders.cs
@@ -0,0 +1,55 @@
+﻿/*
+    Implement a function FolderNames, which accepts a string containing an XML file that specifies folder structure and returns all folder names that start with startingLetter.
+
+    For example, for the letter 'u' and XML file:
+
+    <?xml version="1.0" encoding="UTF-8"?>
+    <folder name="c">
+        <folder name="program files">
+            <folder name="uninstall information" />
+        </folder>
+        <folder name="users" />
+    </folder>
+
+    The function should return "uninstall information" and "users" (in any order).
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+public class Folders
+{
+    public static IEnumerable<string> FolderNames(string xml, char startingLetter)
+    {
+        var doc = XDocument.Parse(xml);
+        var names = from p in doc.Descendants("folder")
+                                where ((string)p.Attribute("name")).StartsWith(startingLetter.ToString())
+                                select (string)p.Attribute("name");
+
+        foreach (string s in names)
+            yield return s;
+    }
+
+    public static void Main(string[] args)
+    {
+        string xml =
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+            "<folder name=\"c\">" +
+                "<folder name=\"program files\">" +
+                    "<folder name=\"uninstall information\" />" +
+                "</folder>" +
+                "<folder name=\"users\" />" +
+            "</folder>";
+
+        /*foreach (string name in FolderNames(xml, 'u'))
+        {
+            Console.WriteLine(name);
+        }*/
+
+        // OR ↓
+
+        FolderNames(xml, 'u').ToList().ForEach(n => Console.WriteLine(n));
+    }
+}
